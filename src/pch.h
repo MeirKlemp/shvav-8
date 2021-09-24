84456f6b767cdbb132501ad9bc10da5ae0357291
Meir Klemfner <meirk251@outlook.com>
2021-09-24 10:55:29 +0300

    feat: add precompile header
    I made glad as a library and linked it to shvav-8 because it is a c code and pch.h is using c++ code

---
+++ src/pch.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <algorithm>
+#include <chrono>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <utility>
+
+/* Data Types */
+#include <array>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
